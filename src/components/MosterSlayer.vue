61b4c4d191e4338a2e9db307866d2ac60e7aaacc
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-11-04 23:15:20 +0200

    change default project

---
+++ src/components/MosterSlayer.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="slayer">
+    <p>
+        For a guide and recipes on how to configure / customize this project,<br>
+        check out the
+        <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'MonsterSlayer',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+
+</style>
